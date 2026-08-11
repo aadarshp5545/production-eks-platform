dcef388017210668827d74a644dfe3d1c83476ea
aadarshp5545 <aadarshp5545@gmail.com>
2026-08-12 00:18:11 +0530

    updted the code with bacckend state file storage bucket

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,13 +1,8 @@
-#terraform {
-
-  #backend "s3" {
-
-    #bucket         = ""
-    #key            = "dev/terraform.tfstate"
-    #region         = "us-east-1"
-    #dynamodb_table = ""
-    #encrypt        = true
-
-  #}
-
-#}
+terraform {
+  backend "s3" {
+    bucket = "eks-adinfra-tfstate"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
+}
