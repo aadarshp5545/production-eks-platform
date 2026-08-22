c37e659d0c72c30f0634fb1d065c274fbdd662a9
aadarshp5545 <aadarshp5545@gmail.com>
2026-08-22 18:55:17 +0530

    Format Terraform configuration

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "eks-adinfra-tfstate"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
+    bucket  = "eks-adinfra-tfstate"
+    key     = "dev/terraform.tfstate"
+    region  = "us-east-1"
     encrypt = true
   }
 }
