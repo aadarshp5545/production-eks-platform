c4ab4a9aca2ee9266a3048e42bfdc589c1ef02a5
aadarshp5545 <aadarshp5545@gmail.com>
2026-08-23 23:37:24 +0530

    removed the local module reference from the eks node group

--- terraform/platform/eks-nodegroup.tf
+++ terraform/platform/eks-nodegroup.tf
@@ -1,22 +1,25 @@
-module "eks_nodegroup" {
-  source = "./modules/eks-nodegroup"
+resource "aws_eks_node_group" "this" {
 
   cluster_name    = module.eks_cluster.cluster_name
   node_group_name = "${local.name}-nodegroup"
-  node_role_arn   = aws_iam_role.eks_node.arn
+
+  node_role_arn = aws_iam_role.eks_node.arn
 
   subnet_ids = [
     data.aws_subnet.private_1.id,
     data.aws_subnet.private_2.id
   ]
 
-  desired_size = 2
-  min_size     = 2
-  max_size     = 4
+  instance_types = local.instance_types
 
-  instance_types = ["t3.medium"]
+  scaling_config {
+    desired_size = 2
+    min_size     = 2
+    max_size     = 4
+  }
 
   tags = merge(local.common_tags, {
     Name = "${local.name}-nodegroup"
   })
+
 }
